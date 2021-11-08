0423ff805ff29ca95793494134cd02ce1eb62b60
kstev <ksteven8646@student.gwinnetttech.edu>
2021-11-08 16:56:58 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.js</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</header>*/}
+
+      {/* 1) Create a Complex JSX Element */}
+      {/*const JSX = <h1>Hello JSX!</h1>;*/}
+
+      {/* 2) Create a Complex JSX Element */}
+
     </div>
   );
 }
